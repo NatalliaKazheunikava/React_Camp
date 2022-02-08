097286b9f44ae20b2f69498d932728529e1435df
NatalliaK <natalliak.dev@gmail.com>
2022-02-07 18:54:42 -0800

    Set up a file to hold action types

---
+++ src/redux/ActionCreators.js
@@ -0,0 +1,11 @@
+import * as ActionTypes from './ActionTypes'; //lets us to import all the named exports from the ActionTypes.js at once
+
+export const addComment = (campsiteId, rating, author, text) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    }
+})
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,7 +1,13 @@
 import { COMMENTS } from '../shared/comments (1)';
+import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_COMMENT:
+            const comment = action.payload;
+            comment.id = state.length;
+            comment.date = new Date().toISOString();
+            return state.concat(comment); //attaches a new item to the array
         default:
             return state;
     }
